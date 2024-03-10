92e4b451c4b522e0e1ba5d1249d2b226cffaeba6
MiguelAngelMonteroFloress <miguel.angel.montero@estudiantat.upc.edu>
2024-03-10 16:46:20 +0100

    RegistroHecho

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Registre from "./screens/Registre";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilo.container}>
+      <Registre />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilo = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
