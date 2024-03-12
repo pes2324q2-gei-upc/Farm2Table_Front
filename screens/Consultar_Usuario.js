1de22e6a1c0a6f93e5aaa2b0ca268b5f2f34d6a9
ivan-parreno <ivan.parreno@estudiantat.upc.edu>
2024-03-12 13:17:43 +0100

    Falta llamadas API

---
+++ screens/Consultar_Usuario.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import {COLORS, SIZES} from "../constants/theme";
+
+const Consultar_Usuario = () => {
+    const [activeButton, setActiveButton] = useState('Productes');
+
+    const onPress = (buttonName) => {
+        setActiveButton(buttonName === activeButton ? '' : buttonName);
+    }
+    const isButtonActive = (buttonName) => {
+        return buttonName === activeButton;
+    }
+
+    const buttonWidth = Dimensions.get('window').width / 3;
+
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text>Info de la persona</Text>
+            </View>
+            {/* Horizontal container for the buttons */}
+            <View style={styles.buttonContainer}>
+                {/* First TouchableOpacity */}
+                <TouchableOpacity
+                    style={[styles.button, { width: buttonWidth, borderColor: isButtonActive('Productes') ? 'orange' : '#1e4d2b' }]}
+                    onPress={() => onPress('Productes')}
+                >
+                    <Text style={[styles.buttonText, { color: isButtonActive('Productes') ? 'orange' : 'white' }]}>Productes</Text>
+                </TouchableOpacity>
+
+                {/* Second TouchableOpacity */}
+                <TouchableOpacity
+                    style={[styles.button, { width: buttonWidth, borderColor: isButtonActive('Ubicació') ? 'orange' : '#1e4d2b' }]}
+                    onPress={() => onPress('Ubicació')}
+                >
+                    <Text style={[styles.buttonText, { color: isButtonActive('Ubicació') ? 'orange' : 'white' }]}>Ubicació</Text>
+                </TouchableOpacity>
+
+                {/* Third TouchableOpacity */}
+                <TouchableOpacity
+                    style={[styles.button, { width: buttonWidth, borderColor: isButtonActive('Sobre mí') ? 'orange' : '#1e4d2b' }]}
+                    onPress={() => onPress('Sobre mí')}
+                >
+                    <Text style={[styles.buttonText, { color: isButtonActive('Sobre mí') ? 'orange' : 'white' }]}>Sobre mí</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.container2}>
+
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: COLORS.secondary,
+        paddingTop: '30%' // Apply padding at the top
+    },
+    container2: {
+        flex: 1,
+        paddingBottom: '200%',
+        backgroundColor: COLORS.primary,
+    },
+    buttonContainer: {
+        flexDirection: 'row', // Aligns items horizontally
+        justifyContent: 'space-between', // Distributes items evenly along the main axis
+        paddingHorizontal: SIZES.padding // Apply horizontal padding
+    },
+    button: {
+        //paddingVertical: 10,
+        alignItems: "center",
+        height: 40,
+        borderBottomWidth: 2,
+    },
+    buttonText: {
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        fontWeight: 'bold',
+        fontSize: SIZES.xlarge
+    },
+});
+
+export default Consultar_Usuario;
