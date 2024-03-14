88e038d6e26b11829560cb86577fff9f1bc4ed49
pau.costafreda <pau.costafreda@estudiantat.upc.edu>
2024-03-14 17:12:14 +0100

    Ole

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { Modal, TouchableOpacity, Text, Button, ScrollView, SafeAreaView, StyleSheet, View, Image, TextInput, Alert } from 'react-native';
+import { TouchableOpacity, Text, ScrollView, SafeAreaView, StyleSheet, View, Image, TextInput, Alert } from 'react-native';
 import { SelectList } from 'react-native-dropdown-select-list';
 import * as ImagePicker from 'expo-image-picker';
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {faCamera} from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
     const [selected, setSelected] = useState('');
@@ -30,7 +32,7 @@ const Home = () => {
     const removeImage = (index) => {
         Alert.alert(
             "Eliminar imagen",
-            "¿Estás seguro de que quieres eliminar esta imagen?",
+            "¿Estàs segur?",
             [
                 {
                     text: "Cancelar",
@@ -48,18 +50,33 @@ const Home = () => {
     };
 
     return (
-        <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.container}>
             <View style={styles.row}>
                 <Text style={styles.name}>Nom del producte:</Text>
+                <TextInput style={styles.writeName} />
+            </View>
+            <View style={styles.row}>
+                <Text style={styles.name}>Descripció:</Text>
                 <TextInput style={styles.writeName} />
             </View>
             <View style={styles.row}>
                 <Text style={styles.name}>Tipus:</Text>
                 <SelectList boxStyles={styles.list} setSelected={setSelected} data={data} save='value' />
             </View>
-            <TouchableOpacity style={styles.button} onPress={pickImage}>
-                <Text style={styles.buttonText}>Pick an image from camera roll</Text>
-            </TouchableOpacity>
+            <View style={styles.row}>
+                <Text style={styles.name}>Quantitat disponible:</Text>
+                <TextInput style={styles.writeName} />
+            </View>
+            <View style={styles.row}>
+                <Text style={styles.name}>Preu</Text>
+                <TextInput style={styles.writeName} />
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={pickImage} style={styles.button}>
+                    <Text style={styles.nameButton}>Inserir imatge</Text>
+                    <FontAwesomeIcon icon={faCamera} size={30} color="#bc6c25" style={styles.icon} />
+                </TouchableOpacity>
+            </View>
             <ScrollView horizontal={true} style={styles.imageScroll}>
                 {images.map((img, index) => (
                     <TouchableOpacity key={index} onPress={() => removeImage(index)} style={styles.imageContainer}>
@@ -67,7 +84,12 @@ const Home = () => {
                     </TouchableOpacity>
                 ))}
             </ScrollView>
-        </SafeAreaView>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={pickImage} style={styles.button}>
+                    <Text style={styles.nameButton}>Afegir producte</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
     );
 };
 
@@ -76,24 +98,35 @@ const styles = StyleSheet.create({
         flex: 1,
         marginBottom: 75,
         marginTop: 20,
-        marginLeft: 15,
+        backgroundColor: "#fefae0",
     },
     name: {
-        color: '#20232a',
+        color: '#ffffff',
         textAlign: 'left',
         fontSize: 20,
         fontWeight: 'bold',
-        marginTop: 2,
+        marginTop: 5,
+    },
+    nameButton: {
+        color: "#ffffff",
+        fontSize: 20,
+        fontWeight: 'bold',
     },
     writeName: {
         marginLeft: 10,
         height: 40,
         borderWidth: 1,
         width: '40%',
+        color: "#ffffff",
     },
     row: {
         flexDirection: 'row',
-        marginVertical: 30,
+        marginVertical: 20,
+        marginLeft: 15,
+        backgroundColor: "#315220",
+        width: '80%',
+        padding: 10,
+        borderRadius: 10,
     },
     list: {
         marginLeft: 10,
@@ -109,6 +142,25 @@ const styles = StyleSheet.create({
     },
     imageContainer: {
         position: 'relative',
+    },
+    icon: {
+        marginLeft: 10,
+    },
+    button: {
+        marginLeft: 15,
+        backgroundColor: "#315220",
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '50%',
+        padding: 10,
+        borderRadius: 10,
+        marginVertical: 20,
+    },
+    buttonContainer: {
+        justifyContent: 'center', // Centra el contenido a lo largo del eje principal (horizontal en este caso)
+        alignItems: 'center', // Centra el contenido a lo largo del eje cruzado (vertical en este caso)
+        width: '100%', // Asegura que el contenedor ocupe el ancho completo
     },
 });
 
