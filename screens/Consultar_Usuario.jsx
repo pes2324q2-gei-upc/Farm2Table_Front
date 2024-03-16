0fff1d2b966d2906d76f116edbf789bf2c61853d
ivan-parreno <ivan.parreno@estudiantat.upc.edu>
2024-03-16 18:17:49 +0100

    Solo falta llamada apis

--- screens/Consultar_Usuario.jsx
+++ screens/Consultar_Usuario.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions, Image} from 'react-native';
 import {COLORS, SIZES} from "../constants/theme";
-import { ScrollView } from 'react-native-gesture-handler';
+import {MaterialIcons, AntDesign} from '@expo/vector-icons';
 
 const Consultar_Usuario = () => {
     const [activeButton, setActiveButton] = useState('Productes');
@@ -17,10 +17,24 @@ const Consultar_Usuario = () => {
 
     return (
         <View style={styles.container}>
-            <View>
-                <Text>Info de la persona Esto ciona muy bien</Text>
+            <View style = {styles.infoContainer}>
+                <View style={styles.imageContainer}>
+                    <Image 
+                        source={require('./descarga.jpg')} 
+                        style={styles.image}
+                    />
+                </View>
+                <View style={styles.info}>
+                    <Text style= {styles.nombre}> Fernando Alonso</Text>
+                    <View style={styles.locationInfo}>
+                        <MaterialIcons name="location-pin" size={(SIZES.width/100)*8} color="white" />
+                        <Text style={styles.locationText}> Ubicació: Madrid</Text>
+                    </View>
+                </View>
+                <View style={styles.ajustes}>
+                    <AntDesign name = "setting" size= {(SIZES.width/100)*10} color = "white" />
+                </View>
             </View>
-            {/* Horizontal container for the buttons */}
             <View style={styles.buttonContainer}>
                 {/* First TouchableOpacity */}
                 <TouchableOpacity
@@ -57,7 +71,7 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: COLORS.secondary,
         flex: 1, 
-        paddingTop: (SIZES.height/100) * 20, // Apply padding at the top
+        paddingTop: (SIZES.height/100) * 8, // Apply padding at the top
     },
     container2: {
         flex: 1,
@@ -81,6 +95,58 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: SIZES.xlarge
     },
+    imageContainer: {
+        width: 110,  // Adjust size as needed
+        height: 110, // Adjust size as needed
+        borderRadius: 75, // Half of the width or height to make it circular
+        overflow: 'hidden', // Clip child components to the rounded border
+        borderWidth: 5, // Border width
+        borderColor: COLORS.tertiary, // Border color
+        position: 'absolute',
+        top: (SIZES.height/100) * 0,
+        left:(SIZES.width/100) * 5,
+      },
+      image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+      },
+      infoContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: (SIZES.height/100) * 14,
+      },
+      info: {
+        position: 'absolute',
+        right: 20,
+        width: '60%',
+        height: '100%',
+        backgroundColor: 'transparent',
+      },
+      ajustes: {
+        position: 'absolute',
+        right:(SIZES.width/100) * 7,
+        top: (SIZES.height/100) * -1,
+        width: '10%',
+        height: '35%',
+        backgroundColor: 'transparent',
+      },
+      nombre: {
+        color: 'white',
+        fontSize:  (SIZES.height/100) * 2,
+        fontWeight: 'bold',
+        paddingBottom:  (SIZES.height/100) * 1,
+      },
+      locationInfo: {
+        flexDirection: 'row', // Aligns icon and text horizontally
+        alignItems: 'center', // Vertically centers the icon
+      },
+      locationText: {
+        color: 'white',
+        fontSize:  (SIZES.height/100) * 2, // Adjust the font size as needed
+        marginLeft: 1,
+      },
 });
 
 export default Consultar_Usuario;
