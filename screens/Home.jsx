4b967b5431fc7ee00bc79f0cf60483e9e7acb8fc
pau.costafreda <pau.costafreda@estudiantat.upc.edu>
2024-03-18 10:18:41 +0100

    Ole

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import { TouchableOpacity, Text, ScrollView, SafeAreaView, StyleSheet, View, Image, TextInput, Alert } from 'react-native';
-import { SelectList } from 'react-native-dropdown-select-list';
+import { Alert, Image, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
-import {faCamera} from "@fortawesome/free-solid-svg-icons";
+import { SelectList } from 'react-native-dropdown-select-list';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCamera, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
     const [selected, setSelected] = useState('');
-    const [images, setImages] = useState([]);
+    const [imageUri, setImageUri] = useState(null);
 
     const data = [
         { key: '1', value: 'Jammu & Kashmir' },
@@ -25,143 +25,166 @@ const Home = () => {
         });
 
         if (!result.canceled) {
-            setImages([...images, result.assets[0].uri]);
+            setImageUri(result.assets[0].uri);
         }
     };
 
-    const removeImage = (index) => {
-        Alert.alert(
-            "Eliminar imagen",
-            "¿Estàs segur?",
-            [
-                {
-                    text: "Cancelar",
-                    style: "cancel"
-                },
-                {
-                    text: "Eliminar", onPress: () => {
-                        let newImages = [...images];
-                        newImages.splice(index, 1);
-                        setImages(newImages);
-                    }
-                }
-            ]
-        );
+    const removeImage = () => {
+        Alert.alert('Eliminar imatge', 'Estàs segur?', [
+            { text: 'Eliminar', onPress: () => setImageUri(null) },
+            { text: 'Cancel·lar', style: 'cancel' },
+        ]);
     };
 
     return (
-        <ScrollView style={styles.container}>
-            <View style={styles.row}>
-                <Text style={styles.name}>Nom del producte:</Text>
-                <TextInput style={styles.writeName} />
-            </View>
-            <View style={styles.row}>
-                <Text style={styles.name}>Descripció:</Text>
-                <TextInput style={styles.writeName} />
-            </View>
-            <View style={styles.row}>
-                <Text style={styles.name}>Tipus:</Text>
-                <SelectList boxStyles={styles.list} setSelected={setSelected} data={data} save='value' />
-            </View>
-            <View style={styles.row}>
-                <Text style={styles.name}>Quantitat disponible:</Text>
-                <TextInput style={styles.writeName} />
-            </View>
-            <View style={styles.row}>
-                <Text style={styles.name}>Preu</Text>
-                <TextInput style={styles.writeName} />
-            </View>
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity onPress={pickImage} style={styles.button}>
-                    <Text style={styles.nameButton}>Inserir imatge</Text>
-                    <FontAwesomeIcon icon={faCamera} size={30} color="#bc6c25" style={styles.icon} />
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollView}>
+                <Text style={styles.title}>Afegir Producte</Text>
+                <InputField label="Nom del producte:" />
+                <InputField label="Descripció:" multiline numberOfLines={4} />
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Tipus:</Text>
+                    <SelectList setSelected={setSelected} data={data} boxStyles={styles.dropdown} dropdownStyles={styles.dropdown} search={true} />
+                </View>
+                <InputField label="Quantitat disponible:" keyboardType="numeric" />
+                <InputField label="Preu per kg:" keyboardType="numeric" />
+                <ImagePickerComponent imageUri={imageUri} pickImage={pickImage} removeImage={removeImage} />
+                <TouchableOpacity style={styles.submitButton}>
+                    <Text style={styles.buttonText}>Afegir producte</Text>
                 </TouchableOpacity>
-            </View>
-            <ScrollView horizontal={true} style={styles.imageScroll}>
-                {images.map((img, index) => (
-                    <TouchableOpacity key={index} onPress={() => removeImage(index)} style={styles.imageContainer}>
-                        <Image source={{ uri: img }} style={styles.image} />
-                    </TouchableOpacity>
-                ))}
             </ScrollView>
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity onPress={pickImage} style={styles.button}>
-                    <Text style={styles.nameButton}>Afegir producte</Text>
-                </TouchableOpacity>
-            </View>
-        </ScrollView>
+        </SafeAreaView>
     );
 };
+
+const InputField = ({ label, multiline, numberOfLines, ...props }) => (
+    <View style={styles.inputContainer}>
+        <Text style={styles.label}>{label}</Text>
+        {multiline ? (
+            <ScrollView style={styles.multilineInputScroll} persistentScrollbar={true}>
+                <TextInput
+                    style={[styles.input, styles.multilineInput]}
+                    multiline={multiline}
+                    numberOfLines={numberOfLines}
+                    textAlignVertical="top"
+                    {...props}
+                />
+            </ScrollView>
+        ) : (
+            <TextInput
+                style={styles.input}
+                multiline={multiline}
+                numberOfLines={numberOfLines}
+                {...props}
+            />
+        )}
+    </View>
+);
+
+const ImagePickerComponent = ({ imageUri, pickImage, removeImage }) => (
+    <View style={styles.imagePickerContainer}>
+        {!imageUri ? (
+            <TouchableOpacity onPress={pickImage} style={styles.imageButton}>
+                <FontAwesomeIcon icon={faCamera} size={20} color="#FFFFFF" />
+                <Text style={styles.buttonText}>Insertar imatge</Text>
+            </TouchableOpacity>
+        ) : (
+            <View style={styles.imagePreviewContainer}>
+                <Image source={{ uri: imageUri }} style={styles.image} />
+                <TouchableOpacity onPress={removeImage} style={styles.removeButton}>
+                    <FontAwesomeIcon icon={faTrash} size={20} color="#FFFFFF" />
+                </TouchableOpacity>
+            </View>
+        )}
+    </View>
+);
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginBottom: 75,
-        marginTop: 20,
-        backgroundColor: "#fefae0",
+        backgroundColor: '#fefae0',
     },
-    name: {
-        color: '#ffffff',
-        textAlign: 'left',
-        fontSize: 20,
+    scrollView: {
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
         fontWeight: 'bold',
-        marginTop: 5,
-    },
-    nameButton: {
-        color: "#ffffff",
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    writeName: {
-        marginLeft: 10,
-        height: 40,
-        borderWidth: 1,
-        width: '40%',
-        color: "#ffffff",
-    },
-    row: {
-        flexDirection: 'row',
+        color: '#315220',
         marginVertical: 20,
-        marginLeft: 15,
-        backgroundColor: "#315220",
-        width: '80%',
+        textAlign: 'center',
+    },
+    inputContainer: {
+        marginBottom: 15,
+    },
+    label: {
+        marginBottom: 5,
+        color: '#315220',
+        fontWeight: 'bold',
+    },
+    input: {
+        backgroundColor: '#FFFFFF',
+        borderColor: '#bc6c25',
+        borderWidth: 1,
+        borderRadius: 5,
         padding: 10,
-        borderRadius: 10,
+        height: 40,
+        textAlignVertical: 'top',
     },
-    list: {
+    multilineInput: {
+        height: 120,
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+    multilineInputScroll: {
+        maxHeight: 150,
+    },
+    dropdown: {
+        borderColor: '#bc6c25',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        backgroundColor: '#FFFFFF',
+    },
+    imagePickerContainer: {
+        marginBottom: 20,
+        alignItems: 'center',
+    },
+    imageButton: {
+        backgroundColor: '#315220',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        borderRadius: 5,
+    },
+    submitButton: {
+        backgroundColor: '#bc6c25',
+        padding: 15,
+        borderRadius: 5,
+        alignItems: 'center',
+        marginTop: 20,
+        marginBottom: 70,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontWeight: 'bold',
         marginLeft: 10,
-        width: 200,
     },
-    imageScroll: {
-        height: 100,
+    imagePreviewContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    removeButton: {
+        marginLeft: 10,
+        backgroundColor: '#E53935',
+        padding: 10,
+        borderRadius: 5,
     },
     image: {
         width: 100,
         height: 100,
-        marginRight: 10,
-    },
-    imageContainer: {
-        position: 'relative',
-    },
-    icon: {
-        marginLeft: 10,
-    },
-    button: {
-        marginLeft: 15,
-        backgroundColor: "#315220",
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '50%',
-        padding: 10,
-        borderRadius: 10,
-        marginVertical: 20,
-    },
-    buttonContainer: {
-        justifyContent: 'center', // Centra el contenido a lo largo del eje principal (horizontal en este caso)
-        alignItems: 'center', // Centra el contenido a lo largo del eje cruzado (vertical en este caso)
-        width: '100%', // Asegura que el contenedor ocupe el ancho completo
+        borderRadius: 5,
     },
 });
-
 export default Home;
