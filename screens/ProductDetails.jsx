0f1125475523ba6daa8909a4701ed30f94a7475b
ribanezro <rafa@tibidabo.org>
2024-05-28 21:10:00 +0200

    ready

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -9,6 +9,7 @@ import { addFavourite, isUserFavourite, removeFavourite } from '../api_service/A
 import { userId, setUserId, userType, getPalabra } from '../informacion/User';
 import CartPopUp from '../PopUps/addedCart';
 import OpenChat from "../components/openChat";
+import addToCart from '../api_service/CartService';
 
 const ProductDetails = ({ navigation, route }) => {
   const [product, setProduct] = useState(null);
@@ -106,38 +107,43 @@ const ProductDetails = ({ navigation, route }) => {
     
 
 
-  const addToCart = async () => {
+  // const addToCart = async () => {
 
-    let cart = []
+  //   let cart = []
 
-    if (!loadCart(userId())) {
-      console.log('No cart found for user:', userId());
-      saveCart(userId(), []);
-    }
-    else {
-      console.log('Cart found for user:', userId());
-      const loadedCart = await loadCart(userId());
-      console.log('Loaded cart:', loadedCart);
-      cart = loadedCart;
-    }
+  //   if (!loadCart(userId())) {
+  //     console.log('No cart found for user:', userId());
+  //     saveCart(userId(), []);
+  //   }
+  //   else {
+  //     console.log('Cart found for user:', userId());
+  //     const loadedCart = await loadCart(userId());
+  //     console.log('Loaded cart:', loadedCart);
+  //     cart = loadedCart;
+  //   }
 
-    const newCartItem = {
-      productId: product.id,
-      name: product.name,
-      quantity: count,
-      price: product.price,
-    };
+  //   const newCartItem = {
+  //     productId: product.id,
+  //     name: product.name,
+  //     quantity: count,
+  //     price: product.price,
+  //   };
 
-    if (cartIconRef.current) {
-      cartIconRef.current.triggerCartAnimation();
-      Vibration.vibrate(200);
-    }
+  //   if (cartIconRef.current) {
+  //     cartIconRef.current.triggerCartAnimation();
+  //     Vibration.vibrate(200);
+  //   }
 
-    console.log('Cart Items:', cart);
-    const updatedCart = addProductToCart(cart, product.productor_info.id, newCartItem, product.productor_info.username, product.productor_info.avatar);
-    console.log('Updated cart:', updatedCart);
-    await saveCart(userId(), updatedCart);
+  //   console.log('Cart Items:', cart);
+  //   const updatedCart = addProductToCart(cart, product.productor_info.id, newCartItem, product.productor_info.username, product.productor_info.avatar);
+  //   console.log('Updated cart:', updatedCart);
+  //   await saveCart(userId(), updatedCart);
+  // }
+
+  const addtoCart = async () => {
+    addToCart(product.id, product.name, product.price, count, product.productor_info.id, product.productor_info.username, product.productor_info.avatar);
   }
+
   return (
     <SafeAreaView style={styles.container}>
       <HeaderBack ref={cartIconRef} />
@@ -184,7 +190,7 @@ const ProductDetails = ({ navigation, route }) => {
                 </View>
 
                 <View style={styles.button_row}>
-                  <TouchableOpacity style={styles.button} onPress={addToCart}>
+                  <TouchableOpacity style={styles.button} onPress={addtoCart}>
                     <Text style={styles.button_text}>Afegir {count} a la cistella</Text>
                   </TouchableOpacity>
                 </View>
